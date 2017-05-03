44a7edc29f569f1604609e3df967c76dab7c38f6
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-03 14:48:50 -0400

    User Review retrieve/update/delete routes work

--- controllers/review.js
+++ controllers/review.js
@@ -3,7 +3,6 @@ var express = require('express');
 var Review = require('../models/review.js');
 var router = express.Router();
 
-
 // ===============  CREATE ROUTE ====================
 router.post('/', function(req,res){
   Review.create(req.body, function(err, newReview){
@@ -11,6 +10,26 @@ router.post('/', function(req,res){
   });
 });
 
+// ===============  RETRIEVE ROUTE ====================
+// Retrieve a review about a restaurant by a user
+router.get('/:userId/:restaurantId', function(req,res){
+  Review.find({userId: req.params.userId, restaurantId:req.params.restaurantId}, function(error, foundReview){
+    res.json(foundReview);
+  });
+});
 
+// =============== UPDATE (PUT) ROUTES ========================
+router.put('/:id', function(req, res){
+  Review.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(error, updatedReview){
+    res.json(updatedReview)
+  });
+});
+
+// =============== DELETE ROUTE =======================
+router.delete('/:id', function(req, res){
+  Review.findByIdAndRemove(req.params.id, function(error, deletedReview){
+    res.json(deletedReview);
+  });
+});
 
 module.exports = router;
